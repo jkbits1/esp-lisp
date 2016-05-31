caaf4e494d86dd994604f10c324f4c287bad27db
jkbits1 <jkbits1@users.noreply.github.com>
2016-05-31 21:08:00 +0100

    fixed bug

--- interrupt.c
+++ interrupt.c
@@ -96,6 +96,32 @@ void checkInterruptQueue()
     		 checkCount = 0;
 
 //    		 printf("item in queue");
+
+			button_ts = btnMsg.now;
+			button_ts *= portTICK_RATE_MS;
+	//
+	//        //if (button04PressCount >= 1) {
+	////          printf("still waiting - %d", button_ts);
+	//        //	button04PressCount = 0;
+	//        //}
+	//
+			if(last < button_ts-200) {
+				printf("interrupt %d fired at %dms\r\n", btnMsg.buttonNumber, button_ts);
+				last = button_ts;
+	//
+				if (btnMsg.buttonNumber == 0) {
+					button00CountChanged = 1;
+					button00PressCount = button00PressCount + 1;
+				}
+	//            if (btnMsg.buttonNumber == 2) {
+	//            	button02CountChanged = 1;
+	//            	button02PressCount = button02PressCount + 1;
+	//            }
+	//            if (btnMsg.buttonNumber == 4) {
+	//            	button04CountChanged = 1;
+	//            	button04PressCount = button04PressCount + 1;
+	//            }
+			}
     	 } else {
     	    checkCount = checkCount + 1;
 
@@ -106,31 +132,6 @@ void checkInterruptQueue()
     	    }
     	 }
 
-        button_ts = btnMsg.now;
-        button_ts *= portTICK_RATE_MS;
-//
-//        //if (button04PressCount >= 1) {
-////          printf("still waiting - %d", button_ts);
-//        //	button04PressCount = 0;
-//        //}
-//
-        if(last < button_ts-200) {
-            printf("interrupt %d fired at %dms\r\n", btnMsg.buttonNumber, button_ts);
-            last = button_ts;
-//
-            if (btnMsg.buttonNumber == 0) {
-            	button00CountChanged = 1;
-            	button00PressCount = button00PressCount + 1;
-            }
-//            if (btnMsg.buttonNumber == 2) {
-//            	button02CountChanged = 1;
-//            	button02PressCount = button02PressCount + 1;
-//            }
-//            if (btnMsg.buttonNumber == 4) {
-//            	button04CountChanged = 1;
-//            	button04PressCount = button04PressCount + 1;
-//            }
-        }
 }
 
 
