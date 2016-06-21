f0b3d5e570a88621a9d155162f14068fc3a644c8
jkbits1 <jkbits1@users.noreply.github.com>
2016-06-21 17:44:39 +0100

    revised definecount

--- lisp.c
+++ lisp.c
@@ -3245,7 +3245,7 @@ int lispreadchar(char *chp) {
 int libLoaded = 0; //1; //0;
 
 int currentDefine = 0;
-int defineCount = 27; //30; // 34;
+int defineCount = 34; //30; // 34;
 
 char *pDefines[] = {
   "(define cols '(red amber green))",
