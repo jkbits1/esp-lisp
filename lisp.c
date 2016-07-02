a08c0760dfd258409800d8fa950b83122145c5e9
jkbits1 <jkbits1@users.noreply.github.com>
2016-07-02 17:27:54 +0100

    increased size of spi data buffer

--- lisp.c
+++ lisp.c
@@ -836,7 +836,7 @@ PRIM spi_test(lisp init, lisp digit, lisp val, lisp decode, lisp delay) {
 	return mkint(1);
 }
 
-int spiData[] = { 0,0,0,0, 0,0,0,0 };
+int spiData[] = { 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0 };
 
 PRIM spi_data(lisp data) {
 
