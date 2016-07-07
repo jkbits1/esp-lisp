bc879d3d5c7d3a9354fcf5dc2c966ddec43a226a
jkbits1 <jkbits1@users.noreply.github.com>
2016-07-07 23:19:09 +0100

    added defines array for word puzzle

--- lisp.c
+++ lisp.c
@@ -3396,7 +3396,56 @@ char *pWheelsDefines[] = {
 //  "(define snd (lambda (xs) (car (cdr xs))) )",
 };
 
-char *pDefines[] = pWheelsDefines;
+char *pWordsDefines[] = {
+  "(define spt (lambda () (led_show 15 8 1 1 5)))",
+  "(define sptt (lambda () (led_show 15 8 1 0 5)))",
+  "(define ledd (lambda () (list (led_data '( 6 6 5 5 ) 0) (led_show 4 0 0 0 5) (led_show 1 0 0 0 5) (led_show 2 0 0 0 5) (sptt) )))",
+  "(ledd)",
+  "(define nth (lambda (n xs) (cond ((eq n 1) (car xs)) (t (nth (- n 1) (cdr xs))))))",
+  "(define drop (lambda (x xs) (cond ((eq x 0) xs) (t (drop (- x 1) (cdr xs))))))",
+  "(define take (lambda (x xs) (cond ((eq x 0) nil) (t (cons (car xs) (take (- x 1) (cdr xs)))))))",
+  "(define append (lambda (xs ys) (if (= (car xs) nil) ys (cons (car xs) (append (cdr xs) ys) ))))",
+  "(define rotate (lambda (n xs) (if (= (car xs) nil) nil (append (drop n xs) (take n xs)))))",
+  "(define incf (lambda (m) (let ((xx (+ (eval m) 1))) (set m xx))))",
+  "(define words 1)",
+  "(set! words '( ( \"fall\" \"fall\" \"fall\" \"fall\" ), ( \"ball\" \"fill\" \"tall\" \"fell\" ), ( \"tell\" \"toll\" \"till\" \"tail\" ), (\"told\" \"roll\" \"loll\" \"poll\"), (\"cold\" \"cold\" \"cold\" \"cold\") ))",
+  "(define curWheel 1)",
+  "(define rotCount '(0 0 0 0 0))",
+  "(define srcHelper (lambda (n v) (append (take (- n 1) rotCount) (cons v (drop n rotCount)))))",
+  "(define setRotCount (lambda (n v) (let ((xx (cond ((eq n 1) (cons v (drop 1 rotCount))) ((eq n 2) (srcHelper n v)) ((eq n 3) (srcHelper n v)) (t (append (take 3 rotCount) (cons v nil))) ))) (set 'rotCount xx))))",
+  "(define loopRotDisp (lambda () (cond ((eq (nth curWheel rotCount) 3) (setRotCount curWheel 0)) (t (setRotCount curWheel (+ (nth curWheel rotCount) 1))))))",
+  "(define loopCurWheel (lambda () (cond ((eq curWheel 5) (set 'curWheel 1)) (t (incf 'curWheel)))))",
+  "(define rotDisp (lambda () (loopRotDisp)))",
+  "(define wheelDisp (lambda () (nth curWheel wheels)))",
+  "(define showDisp (lambda () (list (led_data (nth (nth curWheel rotCount) (wheelDisp))) (ans) (sptt))))",
+  "(interrupt 2 2)",
+  "(interrupt 4 2)",
+  "(define (int02 pin clicks count ms) (list (rotDisp) (showDisp)))",
+  "(define (int04 pin clicks count ms) (list (loopCurWheel) (showDisp)))",
+  "(define wheelShow (lambda (n) (rotate (nth n rotCount) (nth n wheels))))",
+//  "(define zip2 (lambda (xs ys zs) (cond ((eq (car xs) nil) nil) ((eq (car ys) nil) nil) ((eq (car zs) nil) nil) (t (cons (list (car xs) (car ys) (car zs)) (zip2 (cdr xs) (cdr ys) (cdr zs) ) )) ) ))",
+//  "(define sum3 (lambda (t) (+ (+ (car t) (nth 2 t)) (nth 3 t))))",
+//  "(define ans (lambda () (led_data (mapcar sum3 (zip2 (wheelShow 1) (wheelShow 2) (wheelShow 3))) 4) ))",
+  ";",
+  ";",
+  ";"
+  //  "(define rotDisp (lambda () (let ((xx (rotate 1 wheelDisp))) (set 'wheelDisp xx))))",
+//    "(define zip (lambda (xs ys) (cond ((eq (car xs) nil) nil) ((eq (car ys) nil) nil) (t (cons (list (car xs) (car ys)) (zip (cdr xs) (cdr ys)) )) ) ))",
+//    "(define statesNumbered (zip states '(1 2 3 4)) )",
+//    "(define tst (lambda (x) ( if (< 0 x) t nil )))",
+//    "(define tt (lambda (x) t ))",
+//    "(define filterH (lambda (f xs ys) (if (eq xs nil) ys (if (f (car xs)) (filterH f (cdr xs) (cons (car xs) ys)) (filterH f (cdr xs) ys)))))",
+//    "(define filter2 (lambda (f xs) (filterH f xs ())))",
+//    "(define stateByNum (lambda (n) (filter2 (lambda (nls) (eq (car (cdr nls)) n)) statesNumbered)))",
+
+//  "(define (int04 pin clicks count ms) (printf \"b %d cl %d to %d ms %d\" pin clicks count ms))"
+//  "(define rev (lambda (xs) (filterH tt xs ())))",
+//  "(define getNlsNum (lambda (nls) (snd nls)))",
+//  "(define fst (lambda (xs) (car xs)) )",
+//  "(define snd (lambda (xs) (car (cdr xs))) )",
+};
+
+char *pDefines[] = pWordsDefines;
 
 int noFree = 0;
 
