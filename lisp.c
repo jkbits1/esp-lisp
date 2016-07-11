e70f751d9a26b1e420cd14199db6e87ecc859437
jkbits1 <jkbits1@users.noreply.github.com>
2016-07-11 21:41:11 +0100

    various improvements, stopc calls redPattern not redl

--- lisp.c
+++ lisp.c
@@ -3337,13 +3337,13 @@ char *pLightsDefines[] = {
   "(define redld (lambda (n o) (list (redl n) (delay (* o mult)) (clearl))))",
   "(define amberld (lambda (n o) (list (amberl n) (delay (* o mult)) (clearl))))",
   "(define greenld (lambda (n o) (list (greenl n) (delay (* o mult)) (clearl))))",
-  "(define redPattern (lambda () (list (redld 1 50) (delay (* 10 mult)) (redld 1 20) ) ))",
-  "(define amberPattern (lambda () (list (amberld 1 50) (delay (* 10 mult)) (amberld 1 20) ) ))",
-  "(define greenPattern (lambda () (list (greenld 1 50) (delay (* 10 mult)) (greenld 1 20) ) ))",
+  "(define redPattern (lambda (n) (list (redld 1 50) (delay (* 10 mult)) (redld 1 20) ) ))",
+  "(define amberPattern (lambda (n) (list (amberld 1 50) (delay (* 10 mult)) (amberld 1 20) ) ))",
+  "(define greenPattern (lambda (n) (list (greenld 1 50) (delay (* 10 mult)) (greenld 1 20) ) ))",
   "(define lights (lambda (m n o) (list (redl m) (amberl n) (greenl o))))",
   "(list (define clearl (lambda () (lights 0 0 0 ))) (define stopl  (lambda () (lights 1 0 0))) )",
   "(list (define readyl (lambda () (lights 1 1 0))) (define gol    (lambda () (greenPattern))) (define slowl  (lambda () (lights 0 0 1))))",
-  "(list (define stopc  '(redl)) (define readyc '(redl amberl)))",
+  "(list (define stopc  '(redPattern)) (define readyc '(redl amberl)))",
   "(list (define goc    '(greenl)) (define slowc  '(amberl)))",
   "(define states '(stopc readyc goc slowc))",
   "(define incf (lambda (m) (let ((xx (+ (eval m) 1))) (set m xx))))",
@@ -3359,13 +3359,21 @@ char *pLightsDefines[] = {
   "(define upMult   (lambda () (cond ((eq mult 10) (set 'mult 10)) (t (incf 'mult)))))",
   "(define downMult (lambda () (cond ((eq mult 1)  (set 'mult 1))  (t (decf 'mult)))))",
   "(list (interrupt 2 2) (interrupt 4 2))",
-  "(define (int02 pin clicks count ms) (changeLights))",
-  "(define (int04 pin clicks count ms) (upMult))"
+  "(list (define (int02 pin clicks count ms) (downMult)) (define (int04 pin clicks count ms) (upMult)))",
+  "(at -5000 (lambda () (changeLights)))"
 };
+
+// fn from states, e.g. stopc - stopc called with param 1, stopc 1 calls redl with param 1
+
+//"(list (define stopc  '(redl)) (define readyc '(redl amberl)))",
+
 
 //"(define gol    (lambda () (lights 0 0 1)))",
 
 //"(define (int04 pin clicks count ms) (backLights))",
+//(define (int02 pin clicks count ms) (changeLights))
+
+// (at -5000 (lambda () (changeLights)))
 
 char *pWheelsDefines[] = {
 //  "(define spt (lambda () (led_show 15 8 1 1 5)))",
