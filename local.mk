951dfc65f6a343a506f2f9a0fd5cd257b5d41f1a
Jon Kelly <jonkelly@jmkassocs.com>
2016-08-07 01:37:15 +0100

    used function to create filename

--- local.mk
+++ local.mk
@@ -1,6 +1,6 @@
 # makefile additions due to rtos changes
 FW_ADDR_1	= 0x00000 
 FW_ADDR_2	= 0x20000 
-FW_FILE_1    = $(addprefix $(FIRMWARE_DIR),$(FW_ADDR_1).bin) 
-FW_FILE_2    = $(addprefix $(FIRMWARE_DIR),$(FW_ADDR_2).bin) 
+FW_FILE_1    = $(addprefix $(FIRMWARE_DIR),$(addsuffix .bin,FW_ADDR_1)) 
+FW_FILE_2    = $(addprefix $(FIRMWARE_DIR),$(addsuffix .bin,FW_ADDR_2)) 
 
