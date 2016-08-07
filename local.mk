d17c12f230ea33bb422d20e038bcaab4014fdbec
Jon Kelly <jonkelly@jmkassocs.com>
2016-08-07 01:42:39 +0100

    minor fix

--- local.mk
+++ local.mk
@@ -1,6 +1,6 @@
 # makefile additions due to rtos changes
 FW_ADDR_1	= 0x00000 
 FW_ADDR_2	= 0x20000 
-FW_FILE_1    = $(addprefix $(FIRMWARE_DIR),$(addsuffix .bin,FW_ADDR_1)) 
-FW_FILE_2    = $(addprefix $(FIRMWARE_DIR),$(addsuffix .bin,FW_ADDR_2)) 
+FW_FILE_1    = $(addprefix $(FIRMWARE_DIR),$(addsuffix .bin,$(FW_ADDR_1))) 
+FW_FILE_2    = $(addprefix $(FIRMWARE_DIR),$(addsuffix .bin,$(FW_ADDR_2))) 
 
